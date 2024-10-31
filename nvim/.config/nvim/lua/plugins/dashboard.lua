16045dba4ff1def7fea14582f4da65579d52d603
Patrick Champion <patrick.champion@olympe.io>
2024-10-31 13:55:09 +0100

    remove dashboard

--- nvim/.config/nvim/lua/plugins/dashboard.lua
+++ nvim/.config/nvim/lua/plugins/dashboard.lua
@@ -1,8 +1,8 @@
 return {
-	"nvimdev/dashboard-nvim",
-	dependencies = { { "nvim-tree/nvim-web-devicons" } },
-	event = "VimEnter",
-	config = function()
-		require("dashboard").setup()
-	end,
+-- 	"nvimdev/dashboard-nvim",
+-- 	dependencies = { { "nvim-tree/nvim-web-devicons" } },
+-- 	event = "VimEnter",
+-- 	config = function()
+-- 		require("dashboard").setup()
+-- 	end,
 }
